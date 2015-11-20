5f7ff4519cf58775b3365b16cba4b821d66edf82
YassineP <yassinovic12@live.fr>
2015-11-20 21:09:01 +0000

    First Commit

---
+++ D-Sante/D-Sante/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(D_Sante.Startup))]
+namespace D_Sante
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
