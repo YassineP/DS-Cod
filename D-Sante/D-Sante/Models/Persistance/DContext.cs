e3e73f84c5d8da7a5edfd39f055b04bbc3727238
YassineP <yassinovic12@live.fr>
2015-11-22 19:02:33 +0000

    Commit M 1

---
+++ D-Sante/D-Sante/Models/Persistance/DContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using D_Sante.Models.HealthService;
+namespace D_Sante.Models.Persistance
+{
+    public class DContext : DbContext
+    {
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+    }
+}
---
+++ D-Sante/D-Sante/Models/Persistance/Idal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Sante.Models.Persistance
+{
+    interface Idal<T> :IDisposable
+    {
+        void Create();
+    }
+}
