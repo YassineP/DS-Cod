785b6ffb731ae74d1cfccf10bff26d99417caed5
YassineP <yassinovic12@live.fr>
2015-11-22 19:14:37 +0000

    Commit M 2

---
+++ D-Sante/D-Sante/Models/Persitance/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using D_Sante.Models.HealthServices;
+namespace D_Sante.Models.Persitance
+{
+    public class IDbContext : DbContext
+    {
+        public DbSet<Drug> Medicaments { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+    }
+}
