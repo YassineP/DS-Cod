4b3f56a663ad18621880e9b745ddc55ebea53bd2
YassineX <yassine.ouachhal@gmail.com>
2015-11-22 18:44:30 +0000

    Sync 1

---
+++ D-Sante/D-Sante/Models/HealthServices/HealthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace D_Sante.Models.HealthServices
+{
+    public class HealthService
+    {
+    }
+}
