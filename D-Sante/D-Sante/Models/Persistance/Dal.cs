3f4aa628118a5e6c1cb5a52ec23a8aef50e702fe
YassineP <yassinovic12@live.fr>
2015-11-23 11:29:36 +0000

    Commit Dal

--- D-Sante/D-Sante/Models/Persistance/Dal.cs
+++ D-Sante/D-Sante/Models/Persistance/Dal.cs
@@ -44,7 +44,43 @@ namespace D_Sante.Models.Persistance
             Db.SaveChanges();
 
         }
+        public void CreateHospital(Hospital hospital)
+        {
+            Db.Hospitals.Add(hospital);
+            Db.SaveChanges();
+        }
+        public void UpdateHospital(Hospital hospital)
+        {
+            Db.Hospitals.SqlQuery(@"UPDATE Hospital SET Name=@name, Description=@desc,
+            Town=@town, Addesse=@adress WHERE Id=@id", hospital.Name, hospital.Description
+                                  , hospital.Town, hospital.Address, hospital.Id);
+            Db.SaveChanges();
 
+        }
+
+        public Hospital SelectHospitalById(int Id)
+        {
+            return Db.Hospitals.Find(Id);
+        }
+
+
+
+        public void DeleteHospital(Hospital hospital)
+        {
+            Db.Hospitals.Remove(hospital);
+            Db.SaveChanges();
+        }
+
+        public void DeleteHospitalById(int Id)
+        {
+            Db.Hospitals.Remove(Db.Hospitals.Find(Id));
+            Db.SaveChanges();
+        }
+
+        public List<Hospital> ReadHospitals()
+        {
+            return Db.Hospitals.ToList();
+        }
         public void Dispose()
         {
             Db.Dispose();
